6455ed49064d48aa6f1b8000b513d7b3d2462395
tohahpro <tohahpro@gmail.com>
2025-12-11 22:03:02 +0600

    pagination setup

--- src/app/(dashboardLayout)/admin/dashboard/products-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/products-management/page.tsx
@@ -1,5 +1,6 @@
 import ProductManagementHeader from "@/components/modules/Admin/ProductManagement/ProductManagementHeader";
 import ProductsTable from "@/components/modules/Admin/ProductManagement/ProductTable";
+import TablePagination from "@/components/shared/TablePagination";
 import { getAllCategories } from "@/services/admin/categoryManagement";
 import { getProducts } from "@/services/admin/productManagement";
 import { Suspense } from "react";
@@ -7,15 +8,22 @@ import { Suspense } from "react";
 const ProductsManagementPage = async () => {
   const categories = await getAllCategories();
 
-  const productsResults= await getProducts()
+  const productsResults = await getProducts();
+  console.log(productsResults.meta)
+  const totalPages = Math.ceil((productsResults.meta?.total || 1) / (productsResults.meta?.limit || 1));
+    
   return (
     <div>
       <ProductManagementHeader categories={categories.data} />
 
       <Suspense>
         <ProductsTable
-          products={productsResults}
+          products={productsResults.data}
           categories={categories.data}
+        />
+        <TablePagination
+          currentPage={productsResults.meta?.page || 1}
+          totalPages={totalPages || 1}
         />
       </Suspense>
     </div>
